be1eb935a1f1f63ba8740a825f55b6abc92e49a1
Ratnesh Raval <ratneshraval@gmail.com>
2019-06-18 15:44:11 -0700

    removing html mode

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,12 +7,16 @@ require "byebug"
 require "fileutils"
 require "jets"
 require 'active_model_serializers'
+require 'json'
+require 'json_schema'
 
 abort("The Jets environment is running in production mode!") if Jets.env == "production"
 Jets.boot
 
-# require 'rspec/rails'
 require "jets/spec_helpers"
+# require File.expand_path('../config/application', __dir__)
+
+
 
 module Helpers
   def payload(name)
@@ -22,17 +26,16 @@ end
 
 RSpec.configure do |c|
   c.include Helpers
-
-  # c.include ActiveModelSerializers::Test::Schema
+  c.include ActiveModelSerializers::Test::Schema
   # c.include ActiveModelSerializers::Test::Serializer
 end
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
+# Shoulda::Matchers.configure do |config|
+#   config.integrate do |with|
+#     with.test_framework :rspec
 
-    # Keep as many of these lines as are necessary:
-    with.library :active_record
-    with.library :active_model
-  end
-end
+#     # Keep as many of these lines as are necessary:
+#     with.library :active_record
+#     with.library :active_model
+#   end
+# end
